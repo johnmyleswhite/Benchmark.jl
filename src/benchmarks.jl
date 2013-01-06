056f8e44a4baf92ba4f8508d194308711a4517f6
diegozea <diegozea@gmail.com>
2013-01-06 03:51:54 -0200

    Add methods for comparing two functions
    Example use and output:
    
    ```
    julia> e1 = quote
           for i in 1:100
              sum([1:10])
           end
           end
    quote  # line 2:
        for i = 1:100 # line 3:
            sum([1:10])
        end
    end
    
    julia> e2 = quote
           for i in 1:10 
              sum([1:10])
           end
           end
    quote  # line 2:
        for i = 1:10 # line 3:
            sum([1:10])
        end
    end
    
    julia> @benchmark(e1,e2,"compare","mil","cien",300)
    2x12 DataFrame:
            BenchmarkCategory BenchmarkName Iterations  TotalWall AverageWall    MaxWall MinWall             Timestamp            JuliaVersion    JuliaHash CodeHash      OS
    [1,]            "compare"         "mil"        283 1.40667e-5  4.97056e-8 1.19209e-6     0.0 "2013-01-06 02:46:02" "0.0.0+106299916.rdf56" "df564000a7"       NA "Linux"
    [2,]            "compare"        "cien"        317 1.28746e-5  4.06139e-8 1.90735e-6     0.0 "2013-01-06 02:46:02" "0.0.0+106299916.rdf56" "df564000a7"       NA "Linux"
    
    julia> function f()        
           sum([1:1000])
           end
    
    julia> function g()        
           sum([1:100])
           end
    
    julia> benchmark(f,g,"ej","mil","cien",300) 
    2x12 DataFrame:
            BenchmarkCategory BenchmarkName Iterations   TotalWall AverageWall    MaxWall    MinWall             Timestamp            JuliaVersion    JuliaHash CodeHash      OS
    [1,]                 "ej"         "mil"        318 0.000624418  1.96358e-6 1.09673e-5 9.53674e-7 "2013-01-06 02:46:58" "0.0.0+106299916.rdf56" "df564000a7"       NA "Linux"
    [2,]                 "ej"        "cien"        282  7.65324e-5  2.71391e-7 1.19209e-6        0.0 "2013-01-06 02:46:58" "0.0.0+106299916.rdf56" "df564000a7"       NA "Linux"
    ```

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -24,7 +24,57 @@ function benchmark(f::Function, category::String, name::String, N::Int)
   return df
 end
 
+# For comparing two functions
+function benchmark(f::Function, f2::Function, category::String, name::String, name2::String, N::Int)
+  df = DataFrame()
+  choosed = rand(2N) .<= .5
+  N1= sum(choosed)
+  N2= 2N - N1
+  df["BenchmarkCategory"] = [ category; category ] 
+  df["BenchmarkName"] = name
+  df["Iterations"] = N1
+  df[2,"BenchmarkName"] = name2
+  df[2,"Iterations"] = N2
+  times1 = zeros(N1)
+  times2 = zeros(N2)
+  f() # Call once to force JIT compilation
+  f2()
+  i=1
+  j=1
+  for itr in 1:2N
+    if choosed[itr]
+      times1[i] = @elapsed f()
+      i += 1
+    else
+      times2[j] = @elapsed f2()
+      j += 1
+    end
+  end
+  df["TotalWall"] = sum(times1)
+  df["AverageWall"] = mean(times1)
+  df["MaxWall"] = max(times1)
+  df["MinWall"] = min(times1)
+  df[2,"TotalWall"] = sum(times2)
+  df[2,"AverageWall"] = mean(times2)
+  df[2,"MaxWall"] = max(times2)
+  df[2,"MinWall"] = min(times2)
+  df["Timestamp"] = strftime("%Y-%m-%d %H:%M:%S", int(time()))
+  df["JuliaVersion"] = string(VERSION)
+  df["JuliaHash"] = Base.VERSION_COMMIT
+  if isdir(".git")
+    df["CodeHash"] = readchomp(`git rev-parse HEAD`)[1:10]
+  else
+    df["CodeHash"] = NA
+  end
+  df["OS"] = string(OS_NAME)
+  return df
+end
+
+
 benchmark(f::Function, name::String, N::Int) = benchmark(f, name, name, N)
+benchmark(f::Function, f2::Function, name::String, name2::String, N::Int) = benchmark(f, f2, "comparing", name, name2, N)
+benchmark(f::Function, name::String, f2::Function, name2::String, N::Int) = benchmark(f, f2, "comparing", name, name2, N)
+benchmark(f::Function, name::String, f2::Function, name2::String, category::String, N::Int) = benchmark(f, f2, category, name, name2, N)
 
 macro benchmark(ex, category, name, N)
   df = DataFrame()
@@ -40,6 +90,52 @@ macro benchmark(ex, category, name, N)
   df["AverageWall"] = mean(times)
   df["MaxWall"] = max(times)
   df["MinWall"] = min(times)
+  df["Timestamp"] = strftime("%Y-%m-%d %H:%M:%S", int(time()))
+  df["JuliaVersion"] = string(VERSION)
+  df["JuliaHash"] = Base.VERSION_COMMIT
+  if isdir(".git")
+    df["CodeHash"] = readchomp(`git rev-parse HEAD`)[1:10]
+  else
+    df["CodeHash"] = NA
+  end
+  df["OS"] = string(OS_NAME)
+  return df
+end
+
+# For comparing two expressions
+macro benchmark(ex, ex2, category, name, name2, N)
+  df = DataFrame()
+  choosed = rand(2N) .<= .5
+  N1= sum(choosed)
+  N2= 2N - N1
+  df["BenchmarkCategory"] = [ category; category ] 
+  df["BenchmarkName"] = name
+  df["Iterations"] = N1
+  df[2,"BenchmarkName"] = name2
+  df[2,"Iterations"] = N2
+  times1 = zeros(N1)
+  times2 = zeros(N2)
+  esc(ex)
+  esc(ex2)
+  i=1
+  j=1
+  for itr in 1:2N
+    if choosed[itr]
+      times1[i] = @elapsed esc(ex)
+      i += 1
+    else
+      times2[j] = @elapsed esc(ex2)
+      j += 1
+    end
+  end
+  df["TotalWall"] = sum(times1)
+  df["AverageWall"] = mean(times1)
+  df["MaxWall"] = max(times1)
+  df["MinWall"] = min(times1)
+  df[2,"TotalWall"] = sum(times2)
+  df[2,"AverageWall"] = mean(times2)
+  df[2,"MaxWall"] = max(times2)
+  df[2,"MinWall"] = min(times2)
   df["Timestamp"] = strftime("%Y-%m-%d %H:%M:%S", int(time()))
   df["JuliaVersion"] = string(VERSION)
   df["JuliaHash"] = Base.VERSION_COMMIT
