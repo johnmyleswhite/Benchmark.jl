03e29a99955f6f50448e35ccbf2650c3dd4ad001
Jake Rosoman <jkroso@gmail.com>
2016-01-06 18:39:36 +1300

    fix deprecation warnings

--- test/benchmarks.jl
+++ test/benchmarks.jl
@@ -1,11 +1,10 @@
 module TestBenchmarks
-	using Benchmark
+    using Benchmark
 
-	benchmark(() -> 10^2, "Math", "Squaring", 100)
-	benchmark(() -> 10^2, "Squaring", 100)
+    benchmark(() -> 10^2, "Math", "Squaring", 100)
+    benchmark(() -> 10^2, "Squaring", 100)
 
-	benchmarks({
-		 		 {() -> 10^2, "Math", "Squaring", 100},
-		 		 {() -> 10 - 10, "Math", "Subtraction", 100}
-		       })
+    benchmarks(Vector[
+        [() -> 10^2, "Math", "Squaring", 100],
+        [() -> 10 - 10, "Math", "Subtraction", 100]])
 end
